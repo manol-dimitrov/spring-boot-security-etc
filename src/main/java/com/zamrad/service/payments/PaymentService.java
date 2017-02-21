070530a456ab96a087d63a185e3367aab69d0e59
Manol Dimitrov <manol.dimitrov@mendeley.com>
2017-02-21 18:52:22 +0100

    Added payment service to manage stripe objects

---
+++ src/main/java/com/zamrad/service/payments/PaymentService.java
@@ -0,0 +1,23 @@
+package com.zamrad.service.payments;
+
+import com.zamrad.clients.StripeClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+/**
+ * Manages stripe customer and charge objects.
+ */
+@Component
+public class PaymentService {
+
+    @Autowired
+    private StripeClient stripeClient;
+
+    public void createCustomer(){
+
+    }
+
+    public void createCharge(){
+
+    }
+}
